83bf0a3c1f3b969ac07048f267fbb9f32ea9b2dc
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-07-26 15:21:42 -0500

    working thru the overall problem by breaking it down

--- client.js
+++ client.js
@@ -10,3 +10,57 @@ var employees = [ atticus, jem, boo, scout, robert, mayella ];
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 console.log(employees);
+
+
+
+
+//bonus% = reviewrating% + employeenumber% + thisrating%
+
+
+
+// function Bonus(emp) {
+//     this.name = emp.name;
+    
+    
+//     this.bonus% = function {
+//         return this.rate% + this.employ#% - this.annsal%
+    
+//     }
+
+// }
+
+// var jemBonus = new Bonus(jem);
+// console.log(jemBonus);
+
+function calculateBonus (employee) {
+    var bonusPercentage = 0;
+    console.log(employee.annualSalary);
+    
+
+    if (employee.reviewRating <= 2) {
+        bonusPercentage += 0;
+    } else if (employee.reviewRating === 3 ){
+        bonusPercentage += 0.04;
+    } else if (employee.reviewRating === 4 ){
+        bonusPercentage += 0.06;
+    } else if (employee.reviewRating === 5 ){
+        bonusPercentage += 0.10;
+    };
+
+    if (employee.employeeNumber.length === 4) {
+        bonusPercentage += 0.05;
+    }
+
+    if (employee.annualSalary > 65000) {
+        bonusPercentage -= 0.01;
+    }
+
+    var bonus = bonusPercentage * employee.annualSalary;
+    console.log(bonus);
+    
+    bonus += parseInt(employee.annualSalary);
+
+    return bonus; // .05
+}
+
+console.log(calculateBonus(atticus));
